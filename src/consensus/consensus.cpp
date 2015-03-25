2154c9fa1736b3afae55a4dd9e8bbc7d99f1fe73
Eric Voskuil <eric@voskuil.org>
2015-03-25 02:36:23 -0700

    Fix copyleft text.

--- src/consensus/consensus.cpp
+++ src/consensus/consensus.cpp
@@ -3,7 +3,7 @@
  *
  * This file is part of libbitcoin-consensus.
  *
- * libbitcoin-explorer is free software: you can redistribute it and/or
+ * libbitcoin-consensus is free software: you can redistribute it and/or
  * modify it under the terms of the GNU Affero General Public License with
  * additional permissions to the one published by the Free Software
  * Foundation, either version 3 of the License, or (at your option)
