51f857c8f8b0fffb4ee0f5e150db603e54c4c917
evoskuil <eric@voskuil.org>
2015-03-30 11:11:32 -0700

    Generated consensus.hpp and updated version.hpp.

--- include/bitcoin/consensus/version.hpp
+++ include/bitcoin/consensus/version.hpp
@@ -7,6 +7,14 @@
 #ifndef LIBBITCOIN_CONSENSUS_VERSION_HPP
 #define LIBBITCOIN_CONSENSUS_VERSION_HPP
 
+/**
+ * The semantic version of this repository as: [major].[minor].[patch]
+ * For interpretation of the versioning scheme see: http://semver.org
+ */
+
 #define LIBBITCOIN_CONSENSUS_VERSION "1.0.0"
+#define LIBBITCOIN_CONSENSUS_MAJOR_VERSION 1
+#define LIBBITCOIN_CONSENSUS_MINOR_VERSION 0
+#define LIBBITCOIN_CONSENSUS_PATCH_VERSION 0
 
 #endif
