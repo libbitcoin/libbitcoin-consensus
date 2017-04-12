1961ff6ef02a9179a10a10fe358c133ae92cd943
evoskuil <eric@voskuil.org>
2017-04-11 19:59:29 -0700

    Update interface to consensus lib, make deserializer private.

--- src/consensus/consensus.hpp
+++ src/consensus/consensus.hpp
@@ -22,29 +22,10 @@
 #include <cstddef>
 #include <bitcoin/consensus/define.hpp>
 #include <bitcoin/consensus/export.hpp>
-#include "pubkey.h"
 #include "script/script_error.h"
-
-// HACK: this symbol is not portable but is used in one place so hardwire here.
-#ifndef __func__
-#define __func__ "<unknown>"
-#endif
 
 namespace libbitcoin {
 namespace consensus {
-
-// Helper class, not published. This is tested internal to verify_script.
-class BCK_API TxInputStream
-{
-public:
-    TxInputStream(const unsigned char* transaction, size_t transaction_size);
-    TxInputStream& read(char* destination, size_t size);
-
-private:
-    static ECCVerifyHandle secp256k1_context_;
-    const unsigned char* source_;
-    size_t remaining_;
-};
 
 // These are not published in the public header but are exposed here for test.
 BCK_API verify_result_type script_error_to_verify_result(ScriptError_t code);
