781942c3bd18575ce2bfc48073a8687b7cff439e
evoskuil <eric@voskuil.org>
2022-02-26 15:33:00 -0800

    Remove dead code.

--- builds/msvc/build/nuget_all.bat
+++ builds/msvc/build/nuget_all.bat
@@ -10,11 +10,3 @@ ECHO.
 ECHO Downloading libbitcoin vs2017 dependencies from NuGet
 CALL nuget.exe install ..\vs2017\libbitcoin-consensus\packages.config
 CALL nuget.exe install ..\vs2017\libbitcoin-consensus-test\packages.config
-REM ECHO.
-REM ECHO Downloading libbitcoin vs2015 dependencies from NuGet
-REM CALL nuget.exe install ..\vs2015\libbitcoin-consensus\packages.config
-REM CALL nuget.exe install ..\vs2015\libbitcoin-consensus-test\packages.config
-REM ECHO.
-REM ECHO Downloading libbitcoin vs2013 dependencies from NuGet
-REM CALL nuget.exe install ..\vs2013\libbitcoin-consensus\packages.config
-REM CALL nuget.exe install ..\vs2013\libbitcoin-consensus-test\packages.config
