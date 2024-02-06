e317e7773174562cf6899644377596b79c27595c
Phillip Mienk <mienkphi@gmail.com>
2024-02-06 15:41:25 -0800

    Regenerate artifacts.

--- install.sh
+++ install.sh
@@ -368,6 +368,7 @@ set_with_boost_prefix()
         # ax_boost_base.m4 appends /include and adds to BOOST_CPPFLAGS
         # ax_boost_base.m4 searches for /lib /lib64 and adds to BOOST_LDFLAGS
         with_boost="--with-boost=$PREFIX"
+        export BOOST_ROOT="$PREFIX"
     fi
 }
 
@@ -385,6 +386,7 @@ display_configuration()
     display_message "LDFLAGS               : $LDFLAGS"
     display_message "LDLIBS                : $LDLIBS"
     display_message "BUILD_BOOST           : $BUILD_BOOST"
+    display_message "BOOST_ROOT            : $BOOST_ROOT"
     display_message "BUILD_DIR             : $BUILD_DIR"
     display_message "PREFIX                : $PREFIX"
     display_message "DISABLE_SHARED        : $DISABLE_SHARED"
